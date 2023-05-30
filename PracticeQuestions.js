c54cf4aa909597a203a2ad12c3d30e81bedae7db
morelk19 <morelkhristian@gmail.com>
2023-05-30 17:05:25 -0400

    updated questions

--- PracticeQuestions.js
+++ PracticeQuestions.js
@@ -8,13 +8,26 @@ const x = 6
 
 // 1. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
 
+function sum1 (param1 =1, param2=2, param3=x){
+  return param1 + param2 + param3 
+}
+
+
+
 // 2. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
+
+const sum2 =(param1 =4, param2=5, param3=x) =>{
+  return param1 + param2 + param3 
+}
 
 // 3. Write a function that returns another function. (use arrow functions please)
 
+const calculate = (param1=7, param2=8, param3=9, callBackFunc=sum1)=>{
+    return callBackFunc(param1, param2, param3);
+}
 
 // 4. Given the following code explain why the function that returns from getFunction still has access to variable "y" even when "y" is not a global variable.
-
+//JS closures
 
 const getFunction = () => {
   const y = 5;
@@ -38,6 +51,14 @@ const couldThrowError = () => {
   }
   
   return 'success'
+}
+const multiply =(param1=8, param2=16, param3=32, callBackFunc=couldThrowError())=>{
+  try {
+    let couldThrow = couldThrowError();
+    return param1 * param2 * param3; 
+  } catch (error) {
+    console.log("sorry, there was an error")
+  }
 }
 
 
@@ -78,13 +99,37 @@ const userData = [
 // example: [{id: '111', favoriteFoods: 2}]
 
 
+
+
 // 6. Given the data above, use ".reduce" to make an array of all the names
 // of the people who have pizza as one of their favorite foods.
 // example: ['Peter', 'Mary']
 
+//reduce doesn't make arrays.
+
+const pizzaLoverReducer = userData.reduce((list, item)=> {
+  if(item.favorites.food = 'pizza')
+    list += item.name
+    return list
+})
+console.log (pizzaLoverReducer);
+
 
 // 7. Show an an example of a switch statement being used
 
+const fizzBuzzSwitch = (arr) => {
+  return arr.map ( item => {
+      switch (true) {  // does the below statement match the switch
+          case item.name  === 'Mary':
+              return 'FizzBuzz'
+          case item.name  === 'John':
+              return 'Buzz'
+          case item.name  === 'Peter':
+              return 'Fizz'
+      }
+  })
+}
+console.log(fizzBuzzSwitch(userData))
 
 ////////////////////////////////////////////////////////////
 //// OBJECT AND ARRAY DESTRUCTURING
@@ -111,17 +156,24 @@ const userPersonalData = {
 //  accomplishments: ['won award for being good gamer', 'won 1st win', 'got good score on the weekend'],
 // }
 
-
-
+const userCombinedData = {
+  ...userPersonalData, ...userGameData
+}
 
 
 // 9. Make a copy of your new user object but override the birthday to december 31st
- 
+
+const copyUserData = {
+  ...userCombinedData, birthday : "dec 31st"
+}
+
 
 // 10. Use the spread operator to make a copy of the accomplishments array and store it in a new variable
 
-
-
+const accomplishmentArr = {
+  ...userGameData.accomplishments
+}
+console.log(accomplishmentArr);
 
 //  11.Given the object bellow, use object destructuring to get the favorite food value (user.name.favoriteThings.food)
 //  and store it in a variable name food
@@ -135,14 +187,33 @@ var user = {
   },
  };
 
- 
+ let food = {
+  ...user.favoriteThings.food,
+}
+
+
 // 12. Once you have grabbed the favorite foods. Destructure the food array to grab only the first 2 values. //
+let firstTwoFoods = {
+  
+}
+
+
 
 
 // 13. use object destructuring and the rest operator to transform the following array into 3 variables: name, age, and food. 
 //    the food variable should have all the array items starting from the third one.
 const data = ['peter', '34', 'apple', 'oranges', 'pizza', 'tacos'];
 
+const dataStructure = (name=data[0], age=data[1], ...food) => {
+  
+  let newObj = {
+  name: name,
+  age: age,
+  food: [...arr]
+}
+return newObj;
+}
+console.log(dataStructure(data));
 
 
 // 14. use object destructuring to grab the following from the userInfo object: 
